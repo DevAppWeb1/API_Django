07e3e7c4699afcbb2b46b324a51bb2538bcc21e3
imane <imanehadnane@gmail.com>
2022-06-05 20:22:02 +0100

    for test

--- cars_vue/src/views/HomeView.vue
+++ cars_vue/src/views/HomeView.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="home">
-    Home
-  </div>
+    
 </template>
 
 <script>
+
 export default {
   name: 'HomeView',
+  
   components: {
-  }
+  },
+  
 }
+
+
 </script>
+  
+</style>         
+
