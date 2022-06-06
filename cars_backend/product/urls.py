cc97387fc4448755702fa06c030b5ed434a7f2d6
Otmaneadmer <othmanad155@gmai.com>
2022-06-06 11:26:42 +0100

    n

--- cars_backend/product/urls.py
+++ cars_backend/product/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from product import views
 
 urlpatterns = [
-    path('latest-products/', views.LatestProductsList.as_view()),
-   
+  path('latest-products/', views.LatestProductsList.as_view()),
 ]
