f91de2bfab50aabf1b25107b0ebe43800bb96e0a
Khaoula El Hattabi <khaoulaelhattabi00@gmail.com>
2022-06-07 08:42:55 +0100

    Voir un produit -partie1-

--- cars_backend/product/views.py
+++ cars_backend/product/views.py
@@ -1,3 +1,5 @@
+from telnetlib import theNULL
+from django.http import Http404
 from django.shortcuts import render
 
 # from django.db.models import Q
@@ -13,4 +15,19 @@ class LatestProductsList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+
+class ProductDetail(APIView):
+    def get_object (self,category_slug,product_slug):
+        try:
+            return Product.objects.filter(category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+
+    def get_object (self,category_slug,product_slug):
+        products = self.get_object(category_slug,product_slug)
+        serializer = ProductSerializer(products)
         return Response(serializer.data)
